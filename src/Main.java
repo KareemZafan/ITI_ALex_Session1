7d56483e25d39c88622f738f8322dacd731ae2c3
mina-hydro <mina.hany.00k@gmail.com>
2022-10-18 16:02:19 +0200

    changed ip and created anew branch by name for task

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-
-        String ip = "192.168.1.5";
+        //local ip
+        String ip = "127.0.0.1";
         String [] ipSplitters = ip.split("\\.");
         for (String i:ipSplitters){
             System.out.println(i);
