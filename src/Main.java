d2c9ba8e75ec90dddb2b35204e952076468bc793
Hagarhecham <102907429+Hagarhecham@users.noreply.github.com>
2022-10-18 21:18:48 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         String [] ipSplitters = ip.split("\\.");
         for (String i:ipSplitters){
             System.out.println(i);
-        }
+        } else ( System.out.println (ERROR))
 
     }
 }
