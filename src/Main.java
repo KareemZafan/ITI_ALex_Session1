507cba465c206cc6445ed132fa2c3653cfea965d
DESKTOP-OIQ95QP\LENOVO <norhaan.alaa@gmail.com>
2022-10-19 23:43:51 +0200

    norhan

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         String ip = "192.168.1.5";
         String [] ipSplitters = ip.split("\\.");
         for (String i:ipSplitters){
-            System.out.println(i);
+            System.out.println(hhhi);
         }
 
     }
